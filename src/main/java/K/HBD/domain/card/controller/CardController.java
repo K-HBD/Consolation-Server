469745238fceb33e5353c05ed0467ee810c07356
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-22 08:57:34 +0900

    ADD | create CardController

---
+++ src/main/java/K/HBD/domain/card/controller/CardController.java
@@ -0,0 +1,12 @@
+package K.HBD.domain.card.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/card")
+public class CardController {
+
+}
