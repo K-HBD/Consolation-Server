612e8635cf461a6f397d469751e696be133227c3
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-24 14:23:23 +0900

    ADD | AwsS3Controller 추가

---
+++ src/main/java/K/HBD/domain/s3/controller/AwsS3Controller.java
@@ -0,0 +1,25 @@
+package K.HBD.domain.s3.controller;
+
+import K.HBD.domain.s3.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/s3")
+public class AwsS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping("/image")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        return ResponseEntity.ok(awsS3Service.uploadFileFromS3(multipartFile));
+    }
+}
