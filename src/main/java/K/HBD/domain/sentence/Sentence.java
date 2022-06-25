917f3a3cc40a0ddbfcdf09074dcd7a33353c642a
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-25 21:36:25 +0900

    ADD | Sentence Entity 생성

---
+++ src/main/java/K/HBD/domain/sentence/Sentence.java
@@ -0,0 +1,32 @@
+package K.HBD.domain.sentence;
+
+import K.HBD.domain.enumType.Emotion;
+import K.HBD.domain.enumType.Use;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Sentence {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sentence_letter", nullable = false, unique = true)
+    private String letter;
+
+    @Builder.Default
+    @Column(name = "used_letter", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Use used_letter = Use.NOT_USED_LETTER;
+
+    @Column(name = "emotion", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Emotion emotion;
+}
