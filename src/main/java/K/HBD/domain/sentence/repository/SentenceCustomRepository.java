f6fac28ed940a548c3a7509f82a9058abce2466c
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-25 21:39:49 +0900

    ADD | SentenceCustomRepository 추가

---
+++ src/main/java/K/HBD/domain/sentence/repository/SentenceCustomRepository.java
@@ -0,0 +1,9 @@
+package K.HBD.domain.sentence.repository;
+
+import K.HBD.domain.enumType.Emotion;
+import K.HBD.domain.enumType.Use;
+import K.HBD.domain.sentence.Sentence;
+
+public interface SentenceCustomRepository {
+    Sentence findSentenceByUseLetter(Use use, Emotion emotion);
+}
