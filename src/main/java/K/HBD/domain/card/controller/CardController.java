997fe71b20673c531de361cdc04f6e500b039b58
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-26 14:27:17 +0900

    ADD | CardController 추가

--- src/main/java/K/HBD/domain/card/controller/CardController.java
+++ src/main/java/K/HBD/domain/card/controller/CardController.java
@@ -1,6 +1,12 @@
 package K.HBD.domain.card.controller;
 
+import K.HBD.domain.card.dto.CardDto;
+import K.HBD.domain.card.dto.CardResponseDto;
+import K.HBD.domain.card.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/card")
 public class CardController {
 
+    private final CardService cardService;
+
+    @PostMapping()
+    public ResponseEntity<CardResponseDto> takeCard(@RequestBody CardDto card) {
+        return ResponseEntity.ok(cardService.responseCard(card));
+    }
 }
