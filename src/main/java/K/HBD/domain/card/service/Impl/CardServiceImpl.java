0f8f9c59cb1f251c851cb6f36946d91a6cd9efda
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-27 23:35:28 +0900

    FB | 로직 변경

--- src/main/java/K/HBD/domain/card/service/Impl/CardServiceImpl.java
+++ src/main/java/K/HBD/domain/card/service/Impl/CardServiceImpl.java
@@ -4,7 +4,7 @@ import K.HBD.domain.card.dto.CardDto;
 import K.HBD.domain.card.dto.CardResponseDto;
 import K.HBD.domain.card.service.CardService;
 import K.HBD.domain.enumType.Emotion;
-import K.HBD.domain.sentence.Sentence;
+import K.HBD.domain.sentence.repository.SentenceRepository;
 import K.HBD.domain.sentence.service.SentenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,19 +17,20 @@ import javax.transaction.Transactional;
 public class CardServiceImpl implements CardService {
 
     private final SentenceService sentenceService;
+    private final SentenceRepository sentenceRepository;
 
     @Value("${cloud.aws.s3.url}")
-    private final String BASE_URI; // s3에 저장되는 모든 파일의 기본 base url
+    private String BASE_URI; // s3에 저장되는 모든 파일의 기본 base url
 
     @Override
     @Transactional
     public CardResponseDto responseCard(CardDto card) {
         Emotion emotion = Emotion.SAD; // AI 모델 서버에서 가져올 임시 감정
 
-        Sentence sentence = sentenceService.findSentenceByUseLetterIsNotUseLetter(emotion);
+        String letter = sentenceRepository.findLetterByEmotion(emotion);
 
         return CardResponseDto.builder()
-                .sentence(sentence.getLetter())
+                .sentence(letter)
                 .image(BASE_URI + card.getFile())
                 .name(card.getName())
                 .emotion(emotion)
