00664499cd44feb07f68858386161a6b5e19a723
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-27 23:34:48 +0900

    UPD | 생성자 추가

--- src/main/java/K/HBD/domain/sentence/dto/SentenceDto.java
+++ src/main/java/K/HBD/domain/sentence/dto/SentenceDto.java
@@ -1,10 +1,14 @@
 package K.HBD.domain.sentence.dto;
 
 import K.HBD.domain.enumType.Emotion;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class SentenceDto {
     private String letter;
