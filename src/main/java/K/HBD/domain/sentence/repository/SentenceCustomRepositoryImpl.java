68c84d93b12feafa736aa2ab243b0516d823ab2d
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-28 19:38:15 +0900

    FB | limit 을 활용한 fetchOne 조회

--- src/main/java/K/HBD/domain/sentence/repository/SentenceCustomRepositoryImpl.java
+++ src/main/java/K/HBD/domain/sentence/repository/SentenceCustomRepositoryImpl.java
@@ -1,32 +1,27 @@
 package K.HBD.domain.sentence.repository;
 
 import K.HBD.domain.enumType.Emotion;
-import K.HBD.domain.sentence.QSentence;
-import K.HBD.domain.sentence.Sentence;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.querydsl.jpa.impl.JPAUpdateClause;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import static K.HBD.domain.sentence.QSentence.sentence;
 
 
 @RequiredArgsConstructor
 @Repository
 public class SentenceCustomRepositoryImpl implements SentenceCustomRepository {
     private final JPAQueryFactory jpaQueryFactory;
-    private final JPAUpdateClause jpaUpdateClause;
 
     @Override
     public String findLetterByEmotion(Emotion emotion) {
-        List<Sentence> list = jpaQueryFactory.from(QSentence.sentence)
-                .select(QSentence.sentence)
-                .where(QSentence.sentence.emotion.eq(emotion))
-                .orderBy(NumberExpression.random().asc())
-                .fetch();
 
-        String letter = list.stream().map(Sentence::getLetter).findFirst().orElse(null);
-        return letter;
+        return jpaQueryFactory.from(sentence)
+                .select(sentence.letter)
+                .where(sentence.emotion.eq(emotion))
+                .orderBy(NumberExpression.random().asc())
+                .limit(1L)
+                .fetchOne();
     }
 }
