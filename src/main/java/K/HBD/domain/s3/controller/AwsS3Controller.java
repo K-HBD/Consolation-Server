442a2ac2b1a15137d7e95adb62bbae7881bb0830
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-07-05 17:58:07 +0900

    ADD | S3 Delete 추가

--- src/main/java/K/HBD/domain/s3/controller/AwsS3Controller.java
+++ src/main/java/K/HBD/domain/s3/controller/AwsS3Controller.java
@@ -1,25 +1,30 @@
 package K.HBD.domain.s3.controller;
 
+import K.HBD.domain.card.dto.request.ImageDto;
 import K.HBD.domain.s3.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/s3")
+@RequestMapping("v1/api/s3")
 public class AwsS3Controller {
 
     private final AwsS3Service awsS3Service;
 
     @PostMapping("/image")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok(awsS3Service.uploadFileFromS3(multipartFile));
+    public ResponseEntity<String> updateImage(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        return ResponseEntity.ok(awsS3Service.updateImageFromS3(multipartFile));
     }
+
+    @DeleteMapping("/image")
+    public ResponseEntity<String> deleteImage(@RequestBody ImageDto imageDto) {
+        awsS3Service.deleteIMageFromS3(imageDto);
+        return ResponseEntity.ok("Success");
+    }
+
 }
