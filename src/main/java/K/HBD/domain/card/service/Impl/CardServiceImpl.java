d13fc5eecc4705ab34b5a0a9d65b4bf07024bb56
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-07-12 12:19:07 +0900

    test

--- src/main/java/K/HBD/domain/card/service/Impl/CardServiceImpl.java
+++ src/main/java/K/HBD/domain/card/service/Impl/CardServiceImpl.java
@@ -5,6 +5,7 @@ import K.HBD.domain.card.dto.request.EmotionDto;
 import K.HBD.domain.card.dto.request.ImageDto;
 import K.HBD.domain.card.dto.response.CardResponseDto;
 import K.HBD.domain.card.service.CardService;
+import K.HBD.domain.s3.AwsS3Service;
 import K.HBD.domain.sentence.repository.SentenceRepository;
 import K.HBD.domain.sentence.service.SentenceService;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ public class CardServiceImpl implements CardService {
 
     private final SentenceService sentenceService;
     private final SentenceRepository sentenceRepository;
+    private final AwsS3Service awsS3Service;
 
     @Value("${cloud.aws.s3.url}")
     private String BASE_URI; // s3에 저장되는 모든 파일의 기본 base url
@@ -31,7 +33,7 @@ public class CardServiceImpl implements CardService {
     @Transactional
     public CardResponseDto responseCard(CardDto card) {
         EmotionDto emotion = getEmotionFromModel(card.getImage()); // AI 모델 서버에서 가져올 임시 감정
-
+        ddfd
         return CardResponseDto.builder()
                 .sentence(sentenceRepository.findLetterByEmotion(emotion.getEmotion()))
                 .imageUrl(BASE_URI + card.getImage())
@@ -43,7 +45,7 @@ public class CardServiceImpl implements CardService {
     private EmotionDto getEmotionFromModel(String imageUrl) {
         return WebClient.create()
                 .post()
-                .uri("http://10.120.72.237:8082/api/model")
+                .uri("http://43.200.49.41:8082/api/model")
                 .accept(MediaType.APPLICATION_JSON)
                 .body(Mono.just(imageUrl), ImageDto.class)
                 .retrieve()
