56a2ec228dc2fb5a0cc0cadd1ef9538631aab38e
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-07-18 14:45:57 +0900

    ADD | Constructor 추가

--- src/main/java/K/HBD/domain/card/dto/request/EmotionDto.java
+++ src/main/java/K/HBD/domain/card/dto/request/EmotionDto.java
@@ -1,8 +1,15 @@
 package K.HBD.domain.card.dto.request;
 
 import K.HBD.domain.enumType.Emotion;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 public class EmotionDto {
     private Emotion emotion;
